1ca3672f0d4aaa918293c36894742781c74c48e6
yunocrown <jatin2002sep@gmail.com>
2023-10-03 22:43:50 +0530

    added on index.html

--- uploadContent.js
+++ uploadContent.js
@@ -1,23 +1,23 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// Get the file input element.
-const fileInput = document.getElementById('file-input');
+// // Get the file input element.
+// const fileInput = document.getElementById('file-input');
 
-// Add a click event listener to the upload button.
-document.querySelector('.AssetsUploadBtn').addEventListener('click', async () => {
-  // Get the files that the user has selected.
-  const files = fileInput.files;
+// // Add a click event listener to the upload button.
+// document.querySelector('.AssetsUploadBtn').addEventListener('click', async () => {
+//   // Get the files that the user has selected.
+//   const files = fileInput.files;
 
-  // Create a FormData object to store the files.
-  const formData = new FormData();
+//   // Create a FormData object to store the files.
+//   const formData = new FormData();
 
-  // Add the files to the FormData object.
-  for (const file of files) {
-    formData.append('files', file);
-  }
+//   // Add the files to the FormData object.
+//   for (const file of files) {
+//     formData.append('files', file);
+//   }
 
-  // Upload the files to the server.
-  const response = await axios.post('/upload', formData);
+//   // Upload the files to the server.
+//   const response = await axios.post('/upload', formData);
 
-  // Handle the response from the server.
-});
+//   // Handle the response from the server.
+// });
